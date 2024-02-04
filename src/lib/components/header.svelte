4568b524ff901444a6115986ff847f9a19879c52
Andrea Vicarova <andrea.vicarova@gmail.com>
2024-02-04 23:26:38 +0100

    :wheelchair: Accept esc key for page close

--- src/lib/components/header.svelte
+++ src/lib/components/header.svelte
@@ -1,6 +1,9 @@
 <script lang="ts">
+	import { goto } from '$app/navigation';
 	export let title: string;
 	export let previous: string = '/';
+
+	const goBack = (e: KeyboardEvent) => e.key === 'Escape' && (e.preventDefault(), goto(previous));
 </script>
 
 <div class="flex-center">
@@ -8,7 +11,14 @@
 		<h1>{title}</h1>
 		<slot />
 	</div>
-	<a href={previous} class="close" aria-label="Go Back" rel="noopener noreferrer">&times;</a>
+	<a
+		href={previous}
+		class="close"
+		aria-label="Go Back"
+		rel="noopener noreferrer"
+		role="button"
+		on:keydown={goBack}>&times;</a
+	>
 </div>
 
 <style lang="scss">
