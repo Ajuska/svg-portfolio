ff1157a476de6b1f6d7fd8e700300c41f93f1f28
Andrea Vicarova <andrea.vicarova@gmail.com>
2024-02-04 10:14:51 +0100

    :sparkles: Add links to about page

--- src/routes/(app)/about/+page.svelte
+++ src/routes/(app)/about/+page.svelte
@@ -4,11 +4,10 @@
 
 <Header title="Hi, I'm Andrea Vicarova"></Header>
 <p>
-	..and I have a confession to make. Ever since my childhood, I have been captivated by art in every
-	form: paper, clay, plastic, clothes, sound, and eventually, bits. I would never have thought that
-	computers could make those beeping sounds when dialing for an internet connection. Are they alive?
-	Can they hear us? Maybe not, but fast forward, they can definitely fix any issues in this message
-	for me.
+	Ever since my childhood, I have been captivated by art in every form: paper, clay, plastic,
+	clothes, sound, and eventually, bits. I would never have thought that computers could make those
+	beeping sounds when dialing for an internet connection. Are they alive? Can they hear us? Maybe
+	not, but fast forward, they can definitely fix any issues in this message for me.
 </p>
 <p>
 	I'm thrilled that I can speak several human languages, but my brain has also picked up the grammar
@@ -17,4 +16,18 @@
 	Currently, I channel this creativity into crafting pixel-perfect, accessible products for the web
 	or game development.
 </p>
-<p>Feel free to get in touch with me via LinkedIn or email!</p>
+<p>
+	Feel free to get in touch with me via
+	<a
+		href="https://linkedin.com/in/andrea-vicarova"
+		target="_blank"
+		rel="noopener noreferrer"
+		aria-label="LinkedIn (opens in a new tab)">LinkedIn</a
+	>
+	or
+	<a
+		href="https://aemail.com/JZoJ"
+		rel="noopener noreferrer"
+		aria-label="Write Email (opens a new window)">email</a
+	>!
+</p>
